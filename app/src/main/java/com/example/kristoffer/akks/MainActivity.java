ef1348f7ce1bbe58d9f29b39ec6e3616375adb37
kristofferhan <Ook1naij>
2016-10-14 14:55:45 +0200

    endret litt farger

--- app/src/main/java/com/example/kristoffer/akks/MainActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.kristoffer.akks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void lokaler(View v)
+    {
+        Intent intent;
+        switch (v.getId())  {
+            case R.id.lokaler: // R.id.textView1
+                intent = new Intent(this, Second.class);
+        }
+        startActivity(intent)
+    }
+
+
+
 }
