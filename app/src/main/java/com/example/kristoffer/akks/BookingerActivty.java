d4f4920351c86d6663c81d7608c14bbd078e6fac
kristofferhan <Ook1naij>
2016-10-17 14:40:11 +0200

    la til click listeners for alle aktiviteter

---
+++ app/src/main/java/com/example/kristoffer/akks/BookingerActivty.java
@@ -0,0 +1,13 @@
+package com.example.kristoffer.akks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingerActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookinger);
+    }
+}
--- app/src/main/java/com/example/kristoffer/akks/MainActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/MainActivity.java
@@ -16,6 +16,36 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        // Find the View that shows the reserver category
+        TextView reserver = (TextView) findViewById(R.id.reserver);
+
+        // Set a click listener on that View
+        reserver.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ReserverActivity}
+                Intent reserverIntent = new Intent(MainActivity.this, ReserverActivity.class);
+
+                // Start the new activity
+                startActivity(reserverIntent);
+            }
+        });
+
+        // Find the View that shows the bookinger categoty
+        TextView bookinger = (TextView) findViewById(R.id.bookinger);
+
+        // Set a click listener on that View
+        bookinger.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link BookingerActivity}
+                Intent bookingerIntent = new Intent(MainActivity.this, BookingerActivty.class);
+
+                // Start the new activity
+                startActivity(bookingerIntent);
+            }
+        });
+
         // Find the View that shows the lokaler category
         TextView lokaler = (TextView) findViewById(R.id.lokaler);
 
@@ -32,9 +62,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        // Find the View that shows the om categoty
+        TextView om = (TextView) findViewById(R.id.om);
+
+        // Set a click listener on that View
+        om.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link OmActivity}
+                Intent omIntent = new Intent(MainActivity.this, OmActivity.class);
+
+                // Start the new activity
+                startActivity(omIntent);
+            }
+        });
+
     }
-
-
 
 
 }
