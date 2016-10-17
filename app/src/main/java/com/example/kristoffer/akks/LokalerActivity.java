5cf4037a9d2c9aa4a3bb8072cb172535364b62c2
kristofferhan <Ook1naij>
2016-10-17 19:51:53 +0200

    la til click listeners for alle aktiviteter

--- app/src/main/java/com/example/kristoffer/akks/LokalerActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/LokalerActivity.java
@@ -3,11 +3,19 @@ package com.example.kristoffer.akks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class LokalerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lokaler);
+
+
+
+
+
     }
+
 }
