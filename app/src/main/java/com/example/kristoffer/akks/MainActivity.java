9fdc0f5560fcbbea4e79ee8d196c6818301f03d3
kristofferhan <Ook1naij>
2016-10-17 14:10:06 +0200

    la til intent for activity_lokaler

--- app/src/main/java/com/example/kristoffer/akks/MainActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,18 +12,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the lokaler category
+        TextView lokaler = (TextView) findViewById(R.id.lokaler);
+
+        // Set a click listener on that View
+        lokaler.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the lokaler category is clicked
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link LokalerActivity}
+                Intent lokalerIntent = new Intent(MainActivity.this, LokalerActivity.class);
+
+                // Start the new activity
+                startActivity(lokalerIntent);
+            }
+
+        });
     }
 
-    public void lokaler(View v)
-    {
-        Intent intent;
-        switch (v.getId())  {
-            case R.id.lokaler: // R.id.textView1
-                intent = new Intent(this, Second.class);
-        }
-        startActivity(intent)
-    }
 
 
 
