fa4197209d32fa4532a05604148f38e68578c2a9
kristofferhan <kristoffer88@gmail.com>
2016-11-05 21:58:07 +0100

    hei

--- app/src/main/java/com/example/kristoffer/akks/BookingerActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/BookingerActivity.java
@@ -2,12 +2,29 @@ package com.example.kristoffer.akks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingerActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ListView reservasjoner;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_bookinger);
+
+        reservasjoner = (ListView) findViewById(R.id.reservasjoner);
+
+        List<String> reservert = new ArrayList<>();
+        reservert.add("Rom A 1400 1600");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                reservert );
+
+        reservasjoner.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/kristoffer/akks/ReserverActivity.java
+++ app/src/main/java/com/example/kristoffer/akks/ReserverActivity.java
@@ -1,13 +1,13 @@
 package com.example.kristoffer.akks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class ReserverActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserver);
+
     }
 }
