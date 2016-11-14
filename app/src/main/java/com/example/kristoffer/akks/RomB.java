318724566f1af2280818e2e9230f3e86114353a5
kristofferhan <kristoffer88@gmail.com>
2016-11-14 17:21:08 +0100

    innhold på alle aktiviteter

--- app/src/main/java/com/example/kristoffer/akks/RomB.java
+++ app/src/main/java/com/example/kristoffer/akks/RomB.java
@@ -8,6 +8,6 @@ public class RomB extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rom_a);
+        setContentView(R.layout.activity_rom_b);
     }
 }
--- app/src/main/java/com/example/kristoffer/akks/RomC.java
+++ app/src/main/java/com/example/kristoffer/akks/RomC.java
@@ -8,6 +8,6 @@ public class RomC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rom_a);
+        setContentView(R.layout.activity_rom_c);
     }
 }
